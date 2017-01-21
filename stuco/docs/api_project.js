844a6494bf85337a3f8796f007f00a1751af7c97
itotallyrock <itotallyrock@users.noreply.github.com>
2017-01-21 05:39:41 -0600

    Generated basic docs

---
+++ stuco/docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "STUCO-Backend",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-01-21T11:38:57.141Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.3"
+  }
+});
